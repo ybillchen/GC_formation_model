f130ac114e5b29c3442bae729427768cedf34947
Bill Chen (Mac) <ybchen@umich.edu>
2023-03-07 15:21:09 -0500

    updated

--- src/run.py
+++ src/run.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from params import params
 import form
-# import offset
+import offset
 # import assign
 # import get_tid
 # import disrupt
@@ -15,8 +15,8 @@ def run(params):
     run_params = params
     run_params['allcat_name'] = allcat_name
 
-    form.form(run_params)
-    # offset.offset(run_params)
+    # form.form(run_params)
+    offset.offset(run_params)
     # assign.assign(run_params)
     # get_tid.get_tid(run_params)
     # disrupt.disrupt(run_params)
