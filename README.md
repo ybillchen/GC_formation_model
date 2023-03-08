223469aaf94cee96b0f788168b04edebaca0c744
Bill Chen (Mac) <ybchen@umich.edu>
2023-03-08 10:13:34 -0500

    updated

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ A post-processing model for globular cluster formation in cosmological simulatio
 
 ## Background
 
-Placeholder
+This is a long developing project starting from Muratov \& Gnedin (2010) till now. Some important milestones include Li \& Gnedin (2014), Choksi, Gnedin, \& Li (2018), and Chen \& Gnedin (2022). This version is the most recent model by Chen \& Gnedin (2023).
 
 ## Install
 
@@ -24,7 +24,7 @@ Placeholder
 
 ## Maintainers
 
-[@Bill Chen](https://github.com/EnthalpyBill).
+[@Bill Chen](https://github.com/EnthalpyBill)
 
 ## License
 
--- interface/tng.py
+++ interface/tng.py
@@ -12,15 +12,14 @@ def handle_merger_tree(sim_base, save_base, hid, fields=None, dtypes=None):
         dtypes = ['f8', 'i8', 'i8', 'i8', 'i8', 'i8', 'i8', 'i8', 'f8']
 
     tree = il.sublink.loadTree(sim_base, 99, hid, fields, False)
-    
+
     filename = save_base + 'merger_tree_%d.hdf5'%hid
     if os.path.exists(filename):
         os.remove(filename)
 
     with h5py.File(filename, 'w') as f:
         for field, dtype in zip(fields, dtypes):
-            data = tree[field]
-            f.create_dataset(field, data=data, dtype=dtype)
+            f.create_dataset(field, data=tree[field], dtype=dtype)
 
 def handle_halo(sim_base, save_base, hid, parttypes=None, fields_list=None, dtypes_list=None):
 
@@ -48,9 +47,15 @@ def handle_halo(sim_base, save_base, hid, parttypes=None, fields_list=None, dtyp
                 d2 = d.create_group(parttype)
                 cutout = il.snapshot.loadSubhalo(sim_base, s, h, parttype, fields=fields)
 
-                for field, dtype in zip(fields, dtypes):
-                    data = cutout[field]
-                    d2.create_dataset(field, data=data, dtype=dtype)
+                if cutout['count'] == 0: # no particle
+                    d2.create_dataset('count', data=0, dtype='i8')
+                    for field, dtype in zip(fields, dtypes):
+                        d2.create_dataset(field, data=[], dtype=dtype)
+
+                else:
+                    d2.create_dataset('count', data=len(cutout[fields[0]]), dtype='i8')
+                    for field, dtype in zip(fields, dtypes):
+                        d2.create_dataset(field, data=cutout[field], dtype=dtype)
 
 if __name__ == '__main__':
 
@@ -59,5 +64,5 @@ if __name__ == '__main__':
     save_base = '/n/holyscratch01/vogelsberger/billchen/temp/'
     hid = 523889
 
-    handle_merger_tree(sim_base, save_base, hid) 
+    # handle_merger_tree(sim_base, save_base, hid) 
     handle_halo(sim_base, save_base, hid)
