3e58ccb6be6cd7d0f1dd97741c7cb642539a54f5
Bill Chen (Mac) <ybchen@umich.edu>
2023-03-08 10:01:02 -0500

    updated

--- interface/tng.py
+++ interface/tng.py
@@ -1,3 +1,4 @@
+import os
 import h5py
 import illustris_python_true as il
 
@@ -11,8 +12,10 @@ def handle_merger_tree(sim_base, save_base, hid, fields=None, dtypes=None):
         dtypes = ['f8', 'i8', 'i8', 'i8', 'i8', 'i8', 'i8', 'i8', 'f8']
 
     tree = il.sublink.loadTree(sim_base, 99, hid, fields, False)
-
+    
     filename = save_base + 'merger_tree_%d.hdf5'%hid
+    if os.path.exists(filename):
+        os.remove(filename)
 
     with h5py.File(filename, 'w') as f:
         for field, dtype in zip(fields, dtypes):
@@ -34,16 +37,20 @@ def handle_halo(sim_base, save_base, hid, parttypes=None, fields_list=None, dtyp
     tree = il.sublink.loadTree(sim_base, 99, hid, ['SubfindID', 'SnapNum'], False)
 
     filename = save_base + 'halo_%d.hdf5'%hid
+    if os.path.exists(filename):
+        os.remove(filename)
 
     with h5py.File(filename, 'w') as f:
         for h, s in zip(tree['SubfindID'], tree['SnapNum']):
             d = f.create_group('snap_%d_halo_%d'%(s,h))
+
             for parttype, fields, dtypes in zip(parttypes, fields_list, dtypes_list):
+                d2 = d.create_group(parttype)
                 cutout = il.snapshot.loadSubhalo(sim_base, s, h, parttype, fields=fields)
 
                 for field, dtype in zip(fields, dtypes):
                     data = cutout[field]
-                    d.create_dataset(field, data=data, dtype=dtype)
+                    d2.create_dataset(field, data=data, dtype=dtype)
 
 if __name__ == '__main__':
 
