ae840112bfbd4f98930e477eea14b96aac2e466f
Bill Chen (Mac) <ybchen@umich.edu>
2023-03-10 16:27:00 -0500

    updated

--- GC_formation_model/__init__.py
+++ GC_formation_model/__init__.py
@@ -1,9 +1,10 @@
 # Licensed under BSD-3-Clause License - see LICENSE
 
 from . import run
+__all__ = run.__all__
+
 from .run import *
 
-__all__ = run.__all__
 __version__ = '0.0'
 __name__ = 'GC_formation_model'
 __author__ = 'Bill Chen'
