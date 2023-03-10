a44571c06899f798934eb417189d141d0867ba16
Bill Chen (Mac) <ybchen@umich.edu>
2023-03-10 16:31:32 -0500

    updated

--- GC_formation_model/assign.py
+++ GC_formation_model/assign.py
@@ -113,7 +113,7 @@ def assign(params):
             a_s = cutout['GFM_StellarFormationTime']
             sid = cutout['ParticleIDs'].astype(int)
             pos = cutout['Coordinates']
-            print(pos.shape)
+            print(cutout['count'], pos.shape)
             x = pos[:,0] - hpos[0]
             y = pos[:,1] - hpos[1]
             z = pos[:,2] - hpos[2]
